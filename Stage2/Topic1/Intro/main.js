c1766aff5bcf38c6e5a593b0a9085b0b5dde878c
rahimlisarkhan <rahimlisarkhan@gmail.com>
2025-07-19 22:14:54 +0400

    Add initial HTML structure and JavaScript files for Stage 2 Topic 1

---
+++ Stage2/Topic1/Intro/main.js
@@ -0,0 +1,46 @@
+
+// console.log('Hello, welcome to my website!');
+//  var a = 5;
+//  var b = 10;
+
+// var totalPrice = a + b;
+
+//  console.log(totalPrice);
+
+
+// Primitive Data Types in JavaScript
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. Symbol (ES6)
+// 7. BigInt (ES11)
+
+// var fullName = "John Doe"
+// var age = 22
+// var isStudent = false
+// var height = 5.9
+// var weight = 70.5
+// var address = undefined
+// var email = null
+
+// InPrimitive Data Types in JavaScript
+// 1. Object
+// 2. Array
+// 3. Function
+
+
+
+// var name = "John";
+// var surname = "Doe";
+
+// var fullname = name + " " + surname;
+
+// var a = 5.4
+// var b = 10.6;
+// var totalPrice = a - b;
+
+// console.log(fullname);
+// console.log(totalPrice);
+
---
+++ Stage2/Topic1/Intro/operators.js
@@ -0,0 +1,103 @@
+//=======================================
+// 1. Arithmetic Operators
+// +  Addition
+// -  Subtraction
+// *  Multiplication
+// ** Exponentiation (ES2016)
+// /  Division
+// %	Modulus (Division Remainder)
+// ++	Increment
+// --	Decrement
+
+
+// var a = 10;
+// var b = 2;
+
+
+// var addition = a + b;
+// var subtraction = a - b;
+// var multiplication = a * b;
+// var division = a / b;
+// var modulus = a % b;
+// var exponentiation = a ** b;
+
+//  a++;
+//  b--;
+
+// console.log("Addition:", addition);
+// console.log("Subtraction:", subtraction);
+// console.log("Multiplication:", multiplication);
+// console.log("Division:", division);
+// console.log("Modulus:", modulus);
+// console.log("Exponentiation:", exponentiation);
+// console.log("Incremented a:", a);
+// console.log("Decremented b:", b);
+
+
+// var a = 10;
+
+// a = a + 5; 
+
+// console.log(a);
+
+
+// var b = 20;
+
+// var c = 100
+
+// c = c - b;
+
+// console.log(c);
+
+//=======================================
+// JavaScript Assignment Operators
+// =	x = y	x = y
+// +=	x += y	x = x + y
+// -=	x -= y	x = x - y
+// *=	x *= y	x = x * y
+// /=	x /= y	x = x / y
+// %=	x %= y	x = x % y
+// **=	x **= y
+
+// var x = 10;
+// var y = 5;
+
+// y = y + 2
+// y += 2; // Equivalent to y = y + 2
+// y -= 2; // Equivalent to y = y - 2
+// y /= 2; // Equivalent to y = y / 2
+// y *= 2; // Equivalent to y = y * 2
+// y %= 2; // Equivalent to y = y % 2
+// y **= 2; // Equivalent to y = y ** 2
+
+
+// console.log(y);// Output: 7
+
+
+//=======================================
+// JavaScript Comparison Operators
+// ==	equal to
+// ===	equal value and equal type
+// !=	not equal
+// !==	not equal value or not equal type
+// >	greater than
+// <	less than
+// >=	greater than or equal to
+// <=	less than or equal to
+// ?	ternary operator
+
+
+var age = 18
+var limit = 18;
+
+// var result = a < b 
+// var result =  a == b // true
+// var result =  a === b // false
+// var result =  a !== b
+
+var result = age >= limit
+
+console.log(result);
+
+
+
