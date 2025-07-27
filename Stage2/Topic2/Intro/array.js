52b7233cefcd8c3edc821b5b614844eb643df387
rahimlisarkhan <rahimlisarkhan@gmail.com>
2025-07-27 22:22:50 +0400

    Add initial JavaScript and HTML files for Stage 2 Topic 2; implement array and string manipulation examples

---
+++ Stage2/Topic2/Intro/array.js
@@ -0,0 +1,33 @@
+
+
+
+var list = [15, 20, 25, 30, 35, 40, 45, 25, 50];
+// var list = ["+", 994, 10, 310, 38, 97];
+var fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"];
+
+
+// list.push(55); // Add 55 to the end of the list
+// list.push("Banana"); // Add "Banana" to the end of the list
+// var silinenEl =  list.pop(); // Remove the last element from the list
+// list.pop(); // Remove the last element from the list
+// list.shift(); // Remove the first element from the list
+// list.unshift(5); // Add 5 to the beginning of the list
+
+// var result = list.includes(125);
+// var result = list.indexOf(25);
+// var result = list.lastIndexOf(25);
+// var result = list.join(""); // Join the elements of the list into a string
+// list.reverse() // list.sort() // Sort the list in ascending order
+// fruits.sort() // Sort the fruits alphabetically
+
+// var result = list.slice(0, 5); // Get a subarray from index 0 to index 5 (exclusive)
+// var result = list.splice(0, 0,100,101); // Remove the first 5 elements from the list
+// var result = list.splice(2, null,100,101); // Add 100 and 101 at the beginning of the list
+// var result = list.splice(1, 1); // Add 100 and 101 at the beginning of the list
+// var result = list.splice(1, 1, 100, 101); // Add 100 and 101 at the beginning of the list
+
+// var newList = list.concat(fruits); // Concatenate fruits to the list
+
+console.log("Updated list:", list);
+console.log("Result of includes check:", result);
+
---
+++ Stage2/Topic2/Intro/intro.js
@@ -0,0 +1,52 @@
+
+
+
+var list = [15, 20, 25, 30, 35, 40, 45, 50];
+
+var fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"];
+
+var randomList = [1, "Hello", true, null, undefined, [1, 2, 3]];
+
+
+console.log(list);
+
+
+list[0] = 10; // Change the first element to 10
+
+console.log(list);
+
+
+
+// var numberList = randomList[5]; // [1, 2, 3]
+
+// console.log(randomList[5][0]);
+// console.log(numberList[0]);
+
+
+
+// console.log(list.length);
+
+// console.log(fruits[5]);
+
+
+
+
+
+// var element = list[1] //20
+// var element2 = list[6] //45
+
+// var b = 20
+
+// var c = b + element + element2; //20 + 20 + 45 = 85
+
+// console.log(c); //85
+
+
+// console.log("element2",element2);
+
+
+
+
+// console.log("List of numbers:", list);
+// console.log("List of fruits:", fruits);
+// console.log("Random list:", randomList);
