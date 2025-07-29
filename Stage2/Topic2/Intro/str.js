e67ec485721e9629e7824c193b0281cd719158ef
rahimlisarkhan <rahimlisarkhan@gmail.com>
2025-07-29 20:42:05 +0400

    Add heap and string manipulation examples in JavaScript; initialize heap.js and str.js

---
+++ Stage2/Topic2/Intro/str.js
@@ -0,0 +1,88 @@
+
+
+
+var str = "Hello, World! Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+
+// var site = "https://example    ";
+
+
+// var img_url = "https://example.com/image.jpg";
+// var img_url2 = "image.png";
+
+// // console.log(str[0]);
+// console.log(str.length);
+
+
+// str[0] = "h"; // This will not change the string because strings are immutable in JavaScript
+// console.log(str); // Output: "Hello, World!"
+
+
+// var result = str.toUpperCase(); // Convert the string to uppercase
+// var result = str.toLowerCase(); // Convert the string to lowercase
+// var result = str.includes("z"); // Check if the string includes "World"
+// var result = str.indexOf("r"); // Check if the string includes "World"
+// var result = str.lastIndexOf("o"); // Find the last occurrence of "o"
+// var result = str.split(""); // Find the last occurrence of "o"
+// var result = str.split(", "); // Find the last occurrence of "o"
+// var convertList = str.split(" "); // Find the last occurrence of "o"
+// var wordSize = convertList.length; // Find the last occurrence of "o"
+
+
+// var isSecure = site.startsWith("https://");
+// var isSecure2 = site.endsWith(".com");
+
+
+// console.log(isSecure); // Output: true
+// console.log(isSecure2); // Output: true
+
+// if(isSecure == true && isSecure2 == true) {
+//     console.log("The site is secure and ends with .com");
+// } else {
+//     console.log("The site is either not secure or does not end with .com");
+// }
+
+
+// var fullname = "Zahra"
+// var fullname = "AylA"
+// // var fullname = "aylA"
+// // var fullname = "AYlA"
+// // var fullname = "AYLA"
+
+// var convertLowerCase = fullname.toLowerCase(); // Convert the string to lowercase ayla
+
+// var result1 = fullname.startsWith("a"); // Check if the string starts with "A"
+// var result2 = fullname.endsWith("a"); // Check if the string ends with "a"
+
+// console.log(result1);
+// console.log(result2);
+
+
+// if(result1 == true && result2 == true) {
+//     console.log("The name starts with 'A' and ends with 'a'.");
+// }
+// else {
+//     console.log("The name does not start with 'A' or does not end with 'a'.");
+// }
+
+
+// var productCode = "AZ1234567";
+// var productCode = prompt("Enter the product code (e.g., AZ1234567):");
+
+// var isValidCode = productCode.toLowerCase().startsWith("az") && productCode.length === 9;
+
+
+// if(isValidCode) {
+//     console.log("The product code is valid.");
+// }
+// else {
+//     console.log("The product code is invalid.");
+// }
+
+
+
+// var list = [1,2,3,[78,79,[200,201]],10,12,[100,101]]
+
+// var newList = list.flat(2)
+
+// console.log(list);
+// console.log(newList);
