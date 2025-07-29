bdbecab530e81a60008b5667708adb2c6f243340
rahimlisarkhan <rahimlisarkhan@gmail.com>
2025-07-29 22:09:51 +0400

    Refactor main.js; remove commented-out exercises and improve string manipulation examples

--- Stage2/Topic2/Intro/main.js
+++ Stage2/Topic2/Intro/main.js
@@ -1,91 +1,61 @@
 
-// var age = 16
 
-// var a = 10
 
-// if(age >= 18) {
-//     a = 100
+// 1. Exercise: Check if a number is even or odd
+// var number = prompt("Enter a number:");
+
+
+// var result = number % 2; // Check if the number is even or odd 0 - even, 1 - odd
+
+
+// if (result == 0) {
+//     console.log("The number is even.");
+// }
+// else {
+//     console.log("The number is odd.");
 // }
 
+// 2. Exercise: Check if a string starts with a specific letter
+// var str = "a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z"; // input string
 
-// console.log(a);
+// var result = str.toUpperCase()
+
+// var result2 = result.split("+")
+
+// console.log(result);
+// console.log(result2);
+
+// 3. Exercise: Check word length
+
+// var str1 = "Hello"
+// var str2 = "Hello, World!"
+// var str3 ="No results for search term `s`"
+// var str4 = " Hello"
+// var str5 = "  Hello     Bart  "
+
+// var result1 = str1.split(" ").length; // Length of "Hello"
+// var result2 = str2.split(" ").length; // Length of "Hello, World!"
+// var result3 = str3.split(" ").length; // Length of "No results for search term `s`"
+// var result4 = str4.split(" ").length; // Length of " Hello"
+// var result5 = str5.trim().replaceAll(" ", ""); // Length of "  Hello     Bart  "
+
+// console.log(result5);
 
 
-// var age = prompt("Enter your age:"); // Get age from user input
-
-// var a = age >= 18 ? 100 : 10; // Ternary operator for conditional assignment
-
-
-// console.log(a);
+// console.log("Length of str1:", result1); // Output: 1
+// console.log("Length of str2:", result2); // Output: 2
+// console.log("Length of str3:", result3); // Output: 6
+// console.log("Length of str4:", result4); // Output: 2
 
 
-var month = 5; // January
 
-switch(month){
-    case 0:
-        console.log("January");
-        break;
-    case 1:
-        console.log("February");
-        break;
-    case 2:
-        console.log("March");
-        break;
-    case 3:
-        console.log("April");
-        break;
-    case 4:
-        console.log("May");
-        break;
-    case 5:
-        console.log("June");
-        break;
-    case 6:
-        console.log("July");
-        break;
-    case 7:
-        console.log("August");
-        break;
-    case 8:
-        console.log("September");
-        break;
-    case 9:
-        console.log("October");
-        break;
-    case 10:
-        console.log("November");
-        break;
-    case 11:
-        console.log("December");
-        break;
-    default:
-        console.log("Invalid month");
-}
+var str = "world"
 
-// if(month === 0) {
-//     console.log("January");
-// }else if(month === 1) {
-//     console.log("February");
-// }else if(month === 2) {
-//     console.log("March");
-// }else if(month === 3) {
-//     console.log("April");
-// }else if(month === 4) {
-//     console.log("May");
-// }else if(month === 5) {
-//     console.log("June");
-// }else if(month === 6) {
-//     console.log("July");
-// }else if(month === 7) {
-//     console.log("August");
-// }else if(month === 8) {
-//     console.log("September");
-// }else if(month === 9) {
-//     console.log("October");
-// }else if(month === 10) {
-//     console.log("November");
-// }else if(month === 11) {
-//     console.log("December");
-// }else {
-//     console.log("Invalid month");
-// }
+var result = str.split("")
+
+var result2 = result.reverse() // DLROW
+
+var result3 = result2.join("") // WORLD
+console.log(result);
+console.log(result2);
+console.log(result3);
