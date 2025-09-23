9cf9b2825de75f7688759300c0281d0a831f5d91
rahimlisarkhan <rahimlisarkhan@gmail.com>
2025-09-23 21:37:22 +0400

    Add movie fetching functionality with UI enhancements and error handling

---
+++ Stage2/Topic6/promise.js
@@ -0,0 +1,153 @@
+
+
+// //Callback hell - Multiple nested callbacks creating pyramid of doom
+
+// // Basic callback example
+// function A(callback){
+//     console.log("Hello from A");
+//     setTimeout(() => {
+//         console.log("A is done");
+//         callback();
+//     }, 1000);
+// }
+
+// function B(callback){
+//     console.log("Hello from B");
+//     setTimeout(() => {
+//         console.log("B is done");
+//         callback();
+//     }, 500);
+// }
+
+// function C(callback){
+//     console.log("Hello from C");
+//     setTimeout(() => {
+//         console.log("C is done");
+//         callback();
+//     }, 300);
+// }
+
+// console.log("=== Simple Callback Hell Example ===");
+
+// // This creates callback hell - deeply nested callbacks
+// A(() => {
+//     B(() => {
+//         C(() => {
+//             console.log("All functions completed!");
+//         });
+//     });
+// });
+
+// console.log("\n=== More Complex Callback Hell Example ===");
+
+
+
+// function myFetch(){
+
+//     function callback(resolve, reject){
+//         const success = false; // Simulate success or failure
+
+
+//         const data = {
+//             results:[1,2,3,4,5],
+//             total:5
+//         }
+
+//         const error = {
+//             message:"An error occurred"
+//         }
+
+//         setTimeout(() => {
+//             if (success) {
+//                 resolve(data);
+//             } else {
+//                 reject(error);
+//             }
+//         }, 3000);
+//     }
+
+//     const promise = new Promise(callback);
+
+//     return promise;
+// }
+
+// const myPromise = myFetch();
+
+
+// console.log("myPromise",myPromise);
+
+// myPromise.then((message) => {
+//     console.log("Then:", message);
+//     B()
+// }).catch((error) => {
+//     console.log("Catch:", error);
+// }).finally(() => {
+//     console.log("Finally loading sondur");
+// });
+
+// function B(){
+//     console.log("Hello from B");
+// }
+
+
+
+
+function renderData(data){
+
+    const listDiv = document.getElementById("list");
+    listDiv.innerHTML = "Elanlar: <br>";
+    console.log("Data received:",data);
+}
+
+function handleError(error){
+    console.log("Error occurred:",error);
+    const listDiv = document.getElementById("list");
+    listDiv.innerHTML = "An error occurred while fetching data.";
+}
+
+
+
+function myFetch(){
+
+    const myPromiseObject = new Promise((resolve, reject) => {
+        const success = true; // Simulate success or failure
+
+        const data = {
+            results:[1,2,3,4,5],
+            total:5
+        }
+
+        const error = {
+            message:"An error occurred"
+        }
+
+        setTimeout(() => {
+            if (success) {
+                resolve(data);
+            } else {
+                reject(error);
+            }
+        }, 2000);
+    });
+
+return myPromiseObject;
+
+}
+
+
+const myFetchPromise = myFetch();
+
+console.log("myFetchPromise",myFetchPromise);
+
+
+ myFetchPromise.then(()=>{
+    console.log("Then: Promise resolved successfully");
+    renderData();
+ }).catch(()=>{
+    console.log("Catch: Error occurred");
+    handleError();
+ }).finally(()=>{
+    console.log("Finally: Promise settled (either resolved or rejected)");
+ });
+
+
