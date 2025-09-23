b0c9396a6bd949220e8ee8d8dc192b01bbabc9a2
rahimlisarkhan <rahimlisarkhan@gmail.com>
2025-09-23 21:54:22 +0400

    Refactor index.js to remove movie fetching functionality and retain recursive functions for A and factorial

--- Stage2/Topic6/index.js
+++ Stage2/Topic6/index.js
@@ -1,61 +1,28 @@
+//Recursive function
 
-const movieTitleInput = document.getElementById("movieTitle")
-const fetchMovieButton = document.getElementById("fetchMovieButton")
-const loadingEl = document.getElementById("loading")
+function A(num){
+    if(num === 0){
+        return
+    };
+
+    console.log(num);
+    A(num - 1);
+} 
 
 
-function getMovieData(title){
+A(5);// 5 4 3 2 1
 
-const apiKey = "5addd3ac"
-const url =`http://www.omdbapi.com/?apikey=${apiKey}&t=${title}`
 
-const myPromise = fetch(url)
-// console.log("myPromise",myPromise);
-   loadingEl.style.display = "block";
 
-    myPromise.then((response) => {
-        // console.log("Response received:",response);
-        return response.json();
-    }).then((data) => {
-        console.log("Data received:",data);
+function factorial(n){
+    if(n === 0){
+        return 1
+    }
 
-        renderData(data);
-    }).catch((error) => {
-        console.log(error);
-        alert("An error occurred while fetching movie data.");
-        // handleError(error);
-    }).finally(() => {
-        loadingEl.style.display = "none";
-    });
-
+    return n * factorial(n - 1) // 5 * 
 }
 
 
-// getMovieData("o olmasin bu olsun")
+var result = factorial(5) // 5 * 4 * 3 * 2 * 1 = 120
 
-function renderData(data){
-
-    const titleEl = document.querySelector(".movie-title")
-    const genreEl = document.querySelector(".movie-genre")
-    const ratingEl = document.querySelector(".movie-rating")
-    const yearEl = document.querySelector(".movie-year")
-    const posterEl = document.querySelector(".movie-poster")
-    const descriptionEl = document.querySelector(".movie-description")
-
-    titleEl.textContent = data.Title
-    genreEl.textContent = `Genre: ${data.Genre}`
-    yearEl.textContent = `Release Year: ${data.Year}`
-    ratingEl.textContent = `Rating: ${data.imdbRating}/10`
-    descriptionEl.textContent = data.Plot
-    posterEl.src = data.Poster
-
-
-}
-
-
-fetchMovieButton.addEventListener("click", function(){
-    const title = movieTitleInput.value;
-    getMovieData(title)
-
-     movieTitleInput.value = ""
-});
+console.log(result);
